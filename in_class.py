713395b71e84520637aaad4caf880c2a7131f3c7
Bradley May <mayb1991@yahoo.com>
2022-07-14 16:15:53 -0600

    first commit

---
+++ in_class.py
@@ -0,0 +1,27 @@
+
+
+# Create a function that counts how many distinct words are in the string below, 
+# then outputs a dictionary with the words as the key and the value as the amount of times 
+# that word appears in the string.<br>Should output:<br>{'a': 5,<br>
+#  'abstract': 1,<br>
+#  'an': 3,<br>
+#  'array': 2, ... etc...</p>
+
+# a_text = '''
+# In computing, a hash table hash map is a data structure which 
+# implements an associative array abstract data type, a structure that can map keys to values. 
+# A hash table uses a hash function to compute an index into an array of 
+# buckets or slots from which the desired value can be found
+# '''
+
+# def word_count(count):
+#     count = {}
+# for word in [a_text]:
+#     if len(word.split(" ") > 1:)
+#         count = +1
+#     # print(word.split(" ")[10])
+
+
+#     # if word == word:
+#     #     # word_count(count)
+#     #     # count += 1
